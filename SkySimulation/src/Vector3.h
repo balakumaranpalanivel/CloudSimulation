fd59a6897388b8f327cd0d2572485ec160b9e7f9
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-19 11:33:15 +0000

    [MASTER] Added project, Vector3 class and tested it

---
+++ SkySimulation/src/Vector3.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// TODO: Use GLM instead of defining your own Vector Class
+template<typename T>
+class Vector3
+{
+public:
+	Vector3() : x(0), y(0), z(0) {}
+	Vector3(T xx) : x(xx), y(xx), z(xx) {}
+	Vector3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+	Vector3 operator * (const T& r) const { return Vector3(x * r, y * r, z * r); }
+	Vector3 operator * (const Vector3<T> &v) const { return Vector3(x * v.x, y * v.y, z * v.z); }
+	Vector3 operator + (const Vector3& v) const { return Vector3(x + v.x, y + v.y, z + v.z); }
+	Vector3 operator - (const Vector3& v) const { return Vector3(x - v.x, y - v.y, z - v.z); }
+	template<typename U>
+	Vector3 operator / (const Vector3<U>& v) const { return Vector3(x / v.x, y / v.y, z / v.z); }
+	friend Vector3 operator / (const T r, const Vector3& v)
+	{
+		return Vector3(r / v.x, r / v.y, r / v.z);
+	}
+	const T& operator [] (size_t i) const { return (&x)[i]; }
+	T& operator [] (size_t i) { return (&x)[i]; }
+	T length2() const { return x * x + y * y + z * z; }
+	T length() const { return std::sqrt(length2()); }
+	Vector3& operator += (const Vector3<T>& v) { x += v.x, y += v.y, z += v.z; return *this; }
+	Vector3& operator *= (const float& r) { x *= r, y *= r, z *= r; return *this; }
+	friend Vector3 operator * (const float&r, const Vector3& v)
+	{
+		return Vector3(v.x * r, v.y * r, v.z * r);
+	}
+	friend std::ostream& operator << (std::ostream& os, const Vector3<T>& v)
+	{
+		os << v.x << " " << v.y << " " << v.z << std::endl; return os;
+	}
+	T x, y, z;
+};
---
+++ SkySimulation/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Vector3.h"
+
+void main()
+{
+	Vector3<float> test;
+	std::cout << "Hello World\n";
+}
