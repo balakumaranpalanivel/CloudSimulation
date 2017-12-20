eae3991cc32ac5e75ae923f5b2bc730af8b354c1
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-20 08:56:22 +0000

    [MASTER] RaySphereIntersection helper function added for computing incident light

--- SkySimulation/src/Atmosphere.h
+++ SkySimulation/src/Atmosphere.h
@@ -9,12 +9,35 @@ public:
 		float er = 6360e3,
 		float ar = 6420e3,
 		float hr = 7994,
-		float hm = 1200)
-	{}
+		float hm = 1200):
+		mSunDirection(sd),
+		mEarthRadius(er),
+		mAtmoshpereRadius(ar),
+		mHR(hr),
+		mHM(hm)
+	{
+	}
 
 	// Compute Color of Sky for a given camera ray
+	/*
+		This is a volume based rendering model using - Raymarch
+		We first Raymarch along the primary ray - that is the ray which starts
+		from the camera origin and exits the atmosphere or intersect with the
+		planetory body.
+		From each sample along the primary ray. A ray is cast from that point
+		to the Sun and Raymarch along that ray as well. This is to account the
+		effect of the light from sun at different point in the ray from camera
+	*/
 	Vec3f ComputeIncidentLight(const Vec3f& orig, const Vec3f& dir, float tmin, float tmax) const
 	{
+		float t0, t1;
+		// Need not procees computation if the ray does not intersect the atmoshphere
+		if (!Utils::RaySphereIntersect(orig, dir, mAtmoshpereRadius, t0, t1) || t1 < 0)
+		{
+			return 0;
+		}
+
+		// TODO - Remove after implementation
 		return Vec3f(0, 0, 0);
 	}
 
@@ -24,6 +47,9 @@ public:
 	float mHR;					// Thickness (Height) of atmoshphere for Rayleigh Scattering effect
 	float mHM;					// Thickness (Height) of area from earth till where Mie Scattering is observed
 
-	static const Vec3f mBetaR;
-	static const Vec3f mBetaM;
+	static const Vec3f mBetaR;	// Exponential Coefficient for Rayleigh Scattering
+	static const Vec3f mBetaM;	// Exponential Coefficient for Mie Scattering
 };
+
+const Vec3f Atmosphere::mBetaR(3.8e-6f, 13.5e-6f, 33.1e-6f);
+const Vec3f Atmosphere::mBetaM(21e-6f);
---
+++ SkySimulation/src/Utils.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <iostream>
+#include "Atmosphere.h"
+#include "Vector3.h"
+
+namespace Utils
+{
+
+	// Method to solve a quadratic equation based on
+	// co efficients of equation
+	bool SolveQuadratic(float a, float b, float c, float &x1, float &x2)
+	{
+		if (b == 0)
+		{
+			// Handle case where the ray and V are perpendicular
+			// where V = ray.orig - sphere.centre
+			if (a == 0)
+			{
+				return false;
+			}
+
+			x1 = 0;
+			x2 = std::sqrtf(-c / a);
+			return true;
+		}
+
+		float discr = b*b - 4 * a*c;
+		if (discr < 0)
+		{
+			return false;
+		}
+		float q = (b < 0.f) ? -0.5f*(b - std::sqrtf(discr)) : -0.5f*(b + std::sqrtf(discr));
+		x1 = q / a;
+		x2 = c / q;
+
+		return true;
+	}
+
+	// Method to compute the intersection of ray with a sphere
+	// Implemented Analytically considering the equations of a 
+	// ray and sphere.
+	// Ray - mX + c
+	// Sphere - x2 + y2 + z2 = r2
+	// https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+	bool RaySphereIntersect(const Vec3f& orig, const Vec3f& dir, const float& radius, float& t0, float& t1)
+	{
+		float A = dir.x * dir.x + dir.y*dir.y + dir.z*dir.z;
+		float B = 2 * (dir.x * orig.x + dir.y*orig.y + dir.z*orig.z);
+		float C = orig.x * orig.x + orig.y * orig.y + orig.z*orig.z 
+			- radius*radius;
+
+		if (!SolveQuadratic(A, B, C, t0, t1))
+		{
+			return false;
+		}
+
+		// Swap 
+		if (t0 > t1)
+		{
+			std::swap(t0, t1);
+		}
+
+		return true;
+	}
+}
