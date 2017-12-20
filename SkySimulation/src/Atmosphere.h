a049647c44ed8645916484f29f82883f92e44f8e
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-20 09:36:12 +0000

    [MASTER] Impemented the routine to Compute the Incident Light

--- SkySimulation/src/Atmosphere.h
+++ SkySimulation/src/Atmosphere.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Vector3.h"
 
+#ifndef M_PI
+#define M_PI (3.14159265358979323846f)
+#endif
+
 class Atmosphere
 {
 public:
@@ -37,8 +41,78 @@ public:
 			return 0;
 		}
 
-		// TODO - Remove after implementation
-		return Vec3f(0, 0, 0);
+		// Update the view limits
+		if (t0 > tmin && t0 > 0)
+		{
+			tmin = t0;
+		}
+		if (t1 < tmax)
+		{
+			tmax = t1;
+		}
+
+		// The number of times we are marching along the ray?
+		uint32_t numSamples = 16;
+		uint32_t numSamplesLight = 8;
+		float segmentLight = (tmax - tmin) / numSamples;
+		float tCurrent = tmin;
+
+		// Mie and Rayleigh Contribution
+		Vec3f sumR(0), sumM(0);
+		float opticalDepthR = 0, opticalDepthM = 0;
+
+		// mu - Cosine of angle between sun and ray
+		float mu = dot(dir, mSunDirection);
+		// Phase value of Rayleigh
+		float phaseR = 3.f / (16.f*M_PI) * (1 + mu*mu);
+		// Phase value of Mie
+		float g = 0.76f;
+		float phaseM = 3.f / (8.f * M_PI) * ((1.f - g * g) * (1.f + mu * mu)) / ((2.f + g * g) * pow(1.f + g * g - 2.f * g * mu, 1.5f));
+
+		// why numSamples ?? - Integeration
+		for (int i = 0; i < numSamples; ++i)
+		{
+			Vec3f samplePosition = orig + (tCurrent + segmentLight*0.5f)*dir;
+			float height = samplePosition.length() - mEarthRadius;
+			// compute the optical depth for light
+			float hr = exp(-height / mHR)*segmentLight;
+			float hm = exp(-height / mHM)*segmentLight;
+			opticalDepthR += hr;
+			opticalDepthM += hm;
+
+			// Light optical depth
+			float t0Light, t1Light;
+			Utils::RaySphereIntersect(samplePosition, mSunDirection, mAtmoshpereRadius, t0Light, t1Light);
+			float segmentLengthLight = t1Light / numSamplesLight, tCurrentLight = 0;
+			float opticalDepthLightR = 0, opticalDepthLightM = 0;
+			int j;
+			for (j = 0; j < numSamplesLight; j++)
+			{
+				Vec3f samplePositionLight = samplePosition + (tCurrentLight + segmentLengthLight*0.5f)*mSunDirection;
+				float heightLight = samplePositionLight.length() - mEarthRadius;
+				if (heightLight < 0)
+				{
+					break;
+				}
+				opticalDepthLightR += exp(-heightLight / mHR) * segmentLengthLight;
+				opticalDepthLightM += exp(-heightLight / mHM) * segmentLengthLight;
+				tCurrentLight += segmentLengthLight;
+			}
+
+			if (j == numSamplesLight)
+			{
+				// Adding the Rayleight and Mie effects
+				Vec3f tau = (mBetaR * (opticalDepthR + opticalDepthLightR))
+					+ (mBetaM * 1.1f * (opticalDepthM + opticalDepthLightM));
+				Vec3f attenuation(exp(-tau.x), exp(-tau.y), exp(-tau.z));
+				sumR += attenuation * hr;
+				sumM += attenuation * hm;
+			}
+			tCurrent += segmentLight;
+		}
+
+		// Consider the intensity of the sun as 20
+		return (sumR * mBetaR * phaseR * sumM * mBetaM * phaseM) * 20;
 	}
 
 	Vec3f mSunDirection;		// The direction of the Sun (normailised)
