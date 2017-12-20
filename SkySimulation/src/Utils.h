6ad4be0f607bc8e9bcc497b491255a944a5a2905
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-12-20 10:42:55 +0000

    [MASTER] Implemented method to render the sky dome

--- SkySimulation/src/Utils.h
+++ SkySimulation/src/Utils.h
@@ -1,7 +1,14 @@
 ﻿#pragma once
 #include <iostream>
+#include <chrono>
+#include <random>
+#include <limits>
+#include <cmath>
+
 #include "Atmosphere.h"
 #include "Vector3.h"
+
+const float kInfinity = std::numeric_limits<float>::max();
 
 namespace Utils
 {
@@ -61,5 +68,44 @@ namespace Utils
 		}
 
 		return true;
+	}
+
+	// Render the sky dome. 
+	// TODO: Currently Method renders to a file. Change to render to window
+	// TODO: Changing to window means optimising the implementation
+	// TODO: Move this method to a seperate redering class
+	void RenderSkyDome_FishEyeCamera(const Vec3f& sunDir, const char *filename)
+	{
+		Atmosphere atmosphere(sunDir);
+		auto t0 = std::chrono::high_resolution_clock::now();
+		// Image size
+		const unsigned width = 512, height = 512;
+		// Vector to hold the pixels of image
+		Vec3f *image = new Vec3f[width*height], *p = image;
+		// Initialise the image pointer with 0x0
+		memset(image, 0x0, sizeof(Vec3f) * width * height);
+
+		// Main Render loop
+		for (unsigned j = 0; j < height; j++)
+		{
+			// TODO: What is this y?
+			float y = 2.f*(j + 0.5f) / float(height - 1) - 1.f;
+			for (unsigned i = 0; i < width; i++)
+			{
+				// TODO: What is this x?
+				float x = 2.f*(i + 0.5f) / float(width - 1) - 1.f;
+				float z2 = x*x + y*y;
+				if (z2 <= 1)
+				{
+					float phi = std::atan2(y, x);
+					float theta = std::acos(1 - z2);
+					Vec3f dir(sin(theta*cos(phi)), cos(theta), 
+						sin(theta)*sin(phi));
+					// 1 meter above sea level
+					*p = atmosphere.ComputeIncidentLight(Vec3f(0, atmosphere.mEarthRadius + 1, 0), dir, 0, kInfinity);
+				}
+			}
+			fprintf(stderr, "\b\b\b\b\%3d%c", (int)(100 * j / (width - 1)), '%');
+		}
 	}
 }
